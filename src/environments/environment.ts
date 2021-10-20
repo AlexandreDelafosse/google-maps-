6bf29c47a95b81c5d685eab60cd05e5d47066129
Delafosse Alexandre <alex.delaf@outlook.fr>
2021-10-20 19:59:39 +0100

    googlemaps

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapsKey: 'AIzaSyB3wBuU--ijO8UT424nanG0M8jz_-SV3ZE'
 };
 
 /*
